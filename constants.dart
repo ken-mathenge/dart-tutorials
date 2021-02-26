b8cd0b7269f1073b97256d12f360be0bc30f7d9b
Dumbledore <mathenge.healthcloud.co.ke>
2021-02-26 17:33:13 +0300

    constants

---
+++ constants.dart
@@ -0,0 +1,16 @@
+// Constants
+
+void main() {
+  // Final
+  final name = 'Jina Kubwa';
+  // The Object is optional
+  print(name);
+
+  const PI = 3.14;
+  print(PI);
+}
+
+class Sample {
+  final sampleName = 'sample';
+  static const anotherSampleName = 'another sample name';
+}
