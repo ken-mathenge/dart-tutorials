663eb6cc7a1cb7eb745eb8473b93de7615d65e9b
Dumbledore <mathenge.healthcloud.co.ke>
2021-03-07 15:15:59 +0300

    iterators: while loop, do-while loop

---
+++ while.dart
@@ -0,0 +1,17 @@
+// while loop
+void main() {
+  var i = 1;
+  while (i <= 10){
+    print(i);
+        i++;
+  }
+
+  do{
+    print(i);
+    i++;
+  } while (i <= 10);
+
+  // Labels
+  // Break
+  // Continue
+}
